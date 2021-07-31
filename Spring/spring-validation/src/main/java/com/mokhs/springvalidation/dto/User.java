055c2bee485b9b284d0a5a739455dc53ca72efa9
mokhs00 <mokhs00@naver.com>
2021-07-31 22:39:10 +0900

    add user dto

---
+++ Spring/spring-validation/src/main/java/com/mokhs/springvalidation/dto/User.java
@@ -0,0 +1,27 @@
+package com.mokhs.springvalidation.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @Min(1)
+    private Integer age;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @NotNull
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")
+    private String phone;
+}
